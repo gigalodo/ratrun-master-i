2992d4872fc6d62580a32967c5daa608b4f3a256
Your Name <you@example.com>
2023-10-24 20:48:26 +0900

    デプロイ準備main2

---
+++ .output/public/_nuxt/loginSuccess.910f3f78.js
@@ -0,0 +1,1 @@
+import{j as e,e as c,o,f as s}from"./entry.aeeb54ce.js";const t={},n=s("p",null,"ログイン成功！！",-1),r=[n];function _(a,l){return o(),c("div",null,r)}const f=e(t,[["render",_]]);export{f as default};
